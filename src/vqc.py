50714b6f5423a1d1448b65f099174b1528ff8ee6
hazki_san <rikka.muma0129@gmail.com>
2025-12-04 02:45:53 +0900

    add some methods

--- src/vqc.py
+++ src/vqc.py
@@ -10,6 +10,8 @@ from scipy.optimize import minimize
 
 from src.createparam import create_param
 from src.hamiltonian import create_xy_hamiltonian
+from src.encoding import encode
+from src.ansatz import ansatz_list
 
 class IndirectVQC:
 
@@ -29,4 +31,48 @@ class IndirectVQC:
         # Optimization variables
         self.optimization_status: bool = optimization["status"]
 
+        
+        self.circuit = None
     
+    def create_circuit(self):
+
+        """
+        encodingとansatzのところの回路を合体させる
+        """
+        encoding_circuit = encode(
+            n_qubit = self.nqubits,
+            feature = self.feature,
+            params = self.params,
+            depth = self.depth,
+        )
+        ansatz_circuit = ansatz_list(
+            n_qubit = self.nqubits,
+            depth = self.depth,
+            params = self.params,
+            ugateH = self.ugateH,
+            gateset = self.gateset,
+        )
+        circuit = encoding_circuit + ansatz_circuit
+        return self.circuit
+
+    def cost_function(num: int, label_train: List[int], label_result: List[int]) -> float:
+
+        """
+        loss_functionのほうがいいかも
+
+        Args:
+            num (int) : データ数
+            label_train: 正解ラベル列
+            label_result: 測定結果列
+        Returns:
+            cost: 平均二乗誤差をcostとして返す 0,1なので二乗しても変わらなさそうだからしてない
+
+        """
+        cost : float = 0
+
+        for i in range(num):
+            cost += (label_train[i]-label_result[i])
+        
+        cost = cost / num
+
+        return cost
