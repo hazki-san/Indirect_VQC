9ca6cd9753fb0e9009c658d45e04dac78be8d8a8
hazki_san <rikka.muma0129@gmail.com>
2025-12-10 17:41:49 +0900

    change params to param

--- src/ansatz.py
+++ src/ansatz.py
@@ -5,14 +5,14 @@ from qulacs.gate import CZ, RX, RY, RZ, Identity, Y, merge
 
 from .time_evolusion_gate import create_time_evo_unitary
 
-def ansatz_list(n_qubit: int, depth: int, params: list[float], ugateH: Observable, gateset: int) -> QuantumCircuit:
+def ansatz_list(n_qubit: int, depth: int, param: list[float], ugateH: Observable, gateset: int) -> QuantumCircuit:
     """
     VQC ansatz circuit after encoding part
    
     Args:
         n_qubit (int): number of qubits
         depth (int): depth of ansatz circuit
-        params (ndarray): Initial params for rotation gates and time evolusion gate:[t1, t2, ..., td, theta1, ..., theta(d*4)]
+        param (ndarray): Initial params for rotation gates and time evolusion gate:[t1, t2, ..., td, theta1, ..., theta(d*4)]
         ugateH (Observable): `qulacs_core.Observable`, Hamiltonian used in time evolusion gate i.e. exp(-iHt)
         gateset (int): number of gateset  大体1でいいと思う
     
@@ -31,23 +31,23 @@ def ansatz_list(n_qubit: int, depth: int, params: list[float], ugateH: Observabl
 
         for i in range(gateset): #通常1回しか回さないと思う
             # Rotation gate
-            circuit.add_gate(RX(0, params[flag + i]))
-            circuit.add_gate(RX(1, params[flag + i + 1]))
-            circuit.add_gate(RY(0, params[flag + i + 2]))
-            circuit.add_gate(RY(1, params[flag + i + 3]))
+            circuit.add_gate(RX(0, param[flag + i]))
+            circuit.add_gate(RX(1, param[flag + i + 1]))
+            circuit.add_gate(RY(0, param[flag + i + 2]))
+            circuit.add_gate(RY(1, param[flag + i + 3]))
         
         # CZ gate
         circuit.add_gate(CZ(0, 1))
         """
         if d == 0:#encodingの関係でいらないかも １こずつずれる
             t_before = 0
-            t_after = params[depth + d]
+            t_after = param[depth + d]
             time_evo_gate = create_time_evo_unitary(ugateH, t_before, t_after)
             circuit.add_gate(time_evo_gate)
         else:
         """
         t_before = param[depth + d]
-        t_after = params[depth + d + 1]
+        t_after = param[depth + d + 1]
         time_evo_gate = create_time_evo_unitary(ugateH, t_before, t_after)
         circuit.add_gate(time_evo_gate)
 
