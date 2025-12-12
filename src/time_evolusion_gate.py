c3cf11c11112345d76d77aaf868ad98583572d78
hazki_san <rikka.muma0129@gmail.com>
2025-12-12 16:11:04 +0900

    replace code

--- src/time_evolusion_gate.py
+++ src/time_evolusion_gate.py
@@ -2,13 +2,12 @@ import numpy as np
 from qulacs import Observable
 from qulacs.gate import DenseMatrix
 
-"""
-処理があんま分かってないので阿南さんのとarijitさんのを読み込むように
-以下コードは阿南さんの形
-"""
-def create_time_evo_unitary(hamiltonian: Observable, t_before: float, t_after: float):
 
-    """
+"""
+    def create_time_evo_unitary(hamiltonian: Observable, t_before: float, t_after: float):
+
+"""
+"""
     Args:
         hamiltonian: qulacs observable
         t_berfore: before time
@@ -16,8 +15,8 @@ def create_time_evo_unitary(hamiltonian: Observable, t_before: float, t_after: f
     
     Returns:
         a density matrix gate U(t)=exp(-iHt)
-
-    """
+"""
+"""
 
     diag, eigen_vecs = self._hamiltonian.eigh
     time_evol_op = np.dot(
@@ -25,4 +24,37 @@ def create_time_evo_unitary(hamiltonian: Observable, t_before: float, t_after: f
         eigen_vecs.T.conj(),
     )
     return DenseMatrix([i for i in range(self.n_qubits)], time_evol_op)
-    
+"""
+
+diag = None
+eigen_vecs = None
+
+def create_time_evo_unitary(hamiltonian: Observable, t_before: float, t_after: float):
+    """
+    Args:
+        hamiltonian: qulacs obsevable
+        t_berfore: initial time
+        t_after: final time
+
+    Returns:
+        a dense matrix gate U(t) = exp(-iHt)
+    """
+    # Get the qubit number
+    n = hamiltonian.get_qubit_count()
+    # Converting to a matrix
+    H_mat = hamiltonian.get_matrix().toarray()
+
+    # Compute eigenvalues and eigenvectors only once and reuse them
+    global diag, eigen_vecs
+
+    if diag is None or eigen_vecs is None:
+        # print("gloabl diag not found")
+        diag, eigen_vecs = np.linalg.eigh(H_mat)
+
+    # Compute the exponent of diagonalized Hamiltonian
+    exponent_diag = np.diag(np.exp(-1j * (t_after - t_before) * diag))
+
+    # Construct the time evolution operator
+    time_evol_op = np.dot(np.dot(eigen_vecs, exponent_diag), eigen_vecs.T.conj())
+
+    return DenseMatrix([i for i in range(n)], time_evol_op)
