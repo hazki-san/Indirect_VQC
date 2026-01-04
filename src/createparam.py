2616cd6c73a09cf8e6bcaa6ed612618af3d6c7ad
hazki_san <rikka.muma0129@gmail.com>
2026-01-04 14:59:23 +0900

    support encode_type

--- src/createparam.py
+++ src/createparam.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def create_param(depth: int, gateset: int, t_init: float, t_final: float) -> np.ndarray:
+def create_param(depth: int, gateset: int, t_init: float, t_final: float, encode_type: int, feature_num: int) -> np.ndarray:
 
     """
     create initla parameters for the circuit
@@ -10,7 +10,7 @@ def create_param(depth: int, gateset: int, t_init: float, t_final: float) -> np.
 
     Args:
         depth(int): number of depth
-        gateset (int): number og rotation gate set. each gateset has 4 gate.1でいいよ
+        gateset (int): number og rotation gate set. each gateset has 4 gate.1でいいよ -> 撤去
         t_init(float): initial time
         t_final(float): final time
 
@@ -23,19 +23,17 @@ def create_param(depth: int, gateset: int, t_init: float, t_final: float) -> np.
     """
     param = np.array([])
     #Time param
-    time = np.random.uniform(t_init, t_final, depth+1)
+    if encode_type == 1:
+        time = np.random.uniform(t_init, t_final, depth + feature_num) #今のところ+4
+    else:
+        time = np.random.uniform(t_init, t_final, depth+1)
+
     time = np.sort(time)
     param = np.append(param, time)
-    #for i in time:
-    #    param = np.append(param, i)
-    #    #print(f"   t_{i}: {}")
 
     #Theta param
     theta = np.random.random(depth * gateset * 4) * 1e-1
     param = np.append(param, theta)
-
-    #for i in theta:
-    #    param = np.append(param, i)
 
     return param
 
