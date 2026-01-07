cd9010ed5d9282433f04188c7211d3009c5c179f
hazki_san <rikka.muma0129@gmail.com>
2026-01-07 17:14:31 +0900

    remove googlecloud

--- src/database/bigquery/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import BigQueryClient
-from .job_result import create_job_result_table, find_job_result, insert_job_result
-
-__all__ = [
-    "BigQueryClient",
-    "create_job_result_table",
-    "insert_job_result",
-    "find_job_result",
-]
--- src/database/bigquery/sql/__init__.py
+++
@@ -1,5 +0,0 @@
-from .find_job import sql_for_find_job
-
-__all__ = [
-    "sql_for_find_job",
-]
